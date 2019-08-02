d205672b0637e6702b0131bfc9b154e10dc60b73
ElimuStuart <stuartelimu@gmail.com>
2019-08-02 06:15:42 +0300

    created views for all crud operations

---
+++ post/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, UpdateView, DeleteView
+from django.views import View
+
+from .models import Post
+from .forms import PostForm
+
+class PostList(ListView):
+    model = Post
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+
+class PostCreate(FormView):
+    template_name = 'post_create.html'
+    form_class = PostForm
+    success_url = reverse('index')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = '__all__'
+    template_name_suffix = '_update_form'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('index')
+
+
+
+
+
